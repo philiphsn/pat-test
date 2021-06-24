86ffff9a090721346b0d95a8d921868035efbd4a
philiphsn <84082347+philiphsn@users.noreply.github.com>
2021-06-24 23:25:42 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,14 +10,19 @@ input.onButtonPressed(Button.A, function () {
 function doSomething () {
 	
 }
-     let list = [1, 2, 3]
+function doSomething2 () {
+	
+}
+let bb = 0
+let aa = 0
+let list = [1, 2, 3]
 list.length
 game.createSprite(2, 2)
 music.playTone(262, music.beat(BeatFraction.Whole))
-if (true) {led.plot(0, 0)
-	let aa = 12;
-    let bb=2234;
-    
+if (true) {
+    led.plot(0, 0)
+    aa = 12
+    bb = 2234
 } else {
 	
 }
